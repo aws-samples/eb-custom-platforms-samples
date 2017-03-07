e65ceb92e542b2e734581fef1d98fb08a9031bc1
Ubuntu <dharsha@amazon.com>
2017-03-07 21:43:10 +0000

    Minor bug fixes.

--- NodePlatform_RHEL/builder/setup-scripts/00-install-proxy.sh
+++ NodePlatform_RHEL/builder/setup-scripts/00-install-proxy.sh
@@ -1,4 +1,5 @@
-#   Copyright 2016-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#!/bin/bash -xe
+#   Copyright  2016-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
 #
@@ -6,7 +7,7 @@
 #
 #   or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-#!/bin/bash
+
 
 . $BUILDER_DIR/CONFIG
 yum install -y nginx
@@ -14,5 +15,5 @@ chkconfig nginx on
 service nginx stop
 
 if [ -e /etc/nginx/conf.d/default.conf ]; then
-	rm -f /etc/nginx/conf.d/default.conf
+    rm -f /etc/nginx/conf.d/default.conf
 fi
--- TomcatPlatform_Ubuntu/builder/builder.sh
+++ TomcatPlatform_Ubuntu/builder/builder.sh
@@ -1,4 +1,5 @@
-#   Copyright 2016-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#!/bin/bash -xe
+#   Copyright   2016-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
 #
@@ -6,12 +7,11 @@
 #
 #   or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-#!/bin/bash -xe
+
 
 BUILDER_DIR="/tmp/builder/"
 
 . $BUILDER_DIR/CONFIG
-
 
 wait_for_cloudinit() {
     echo "Waiting for cloud init to finish bootstrapping.."
